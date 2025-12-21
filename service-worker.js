29cd998f52afabd6c300981e8e10676be166c033
Oliver Kerschhaggl <oliver.kerschhaggl@t-online.de>
2025-12-21 23:46:39 +0100

    Add service worker and manifest for offline support

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'connect4-v1';
+const ASSETS_TO_CACHE = [
+  './connect4.html',
+  './manifest.json',
+  // If you have local images, add them here
+];
+
+// Install event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
